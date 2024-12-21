4d3a46311ed748dba2c77bdcc7edb6205efde4cc
mujdatdurmaz <49032422+mujdatdurmaz@users.noreply.github.com>
2024-12-21 15:16:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.beta_set_page_config(page_title='Müjdat Durmaz', page_icon = favicon, layout = 'wide', initial_sidebar_state = 'auto')
+st.beta_set_page_config(page_title='Müjdat Durmaz')
 
 st.header('Müjdat Durmaz')
 st.subheader('Python ve streamlit çalışmaları')
